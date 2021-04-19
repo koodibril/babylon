bede3f49e427230919630e21c344e21dc27b9f44
AlphaBril <florian.marie.doucet@gmail.com>
2021-04-19 20:40:44 +0200

    finished bazooka

--- html/js/Game.js
+++ html/js/Game.js
@@ -7,20 +7,34 @@ Game = function(canvasId) {
     // Canvas et engine défini ici
     var canvas = document.getElementById(canvasId);
     var engine = new BABYLON.Engine(canvas, true);
+    this.engine = engine;
     var _this = this;
-    
-    // On initie la scène avec une fonction associé à l'objet Game
+    _this.actualTime = Date.now();
+    // On initie la scène avec une fonction associée à l'objet Game
     this.scene = this._initScene(engine);
-
-    var _player = new Player(_this, canvas);
-    var _arena = new Arena(_this);
     
+    var _player = new Player(_this, canvas);
+
+    var _arena = new Arena(_this);
+
     // Permet au jeu de tourner
     engine.runRenderLoop(function () {
+
+        // Récuperet le ratio par les fps
+        _this.fps = Math.round(1000/engine.getDeltaTime());
+
+        // Checker le mouvement du joueur en lui envoyant le ratio de déplacement
+        _player._checkMove((_this.fps)/60);
+
         _this.scene.render();
+        
+        // Si launchBullets est a true, on tire
+        if(_player.camera.weapons.launchBullets === true){
+            _player.camera.weapons.launchFire();
+        }
     });
 
-    // Ajuste la vue 3D si la fenetre est agrandi ou diminué
+    // Ajuste la vue 3D si la fenetre est agrandie ou diminuée
     window.addEventListener("resize", function () {
         if (engine) {
             engine.resize();
@@ -38,3 +52,18 @@ Game.prototype = {
         return scene;
     }
 };
+
+// ------------------------- TRANSFO DE DEGRES/RADIANS 
+function degToRad(deg)
+{
+   return (Math.PI*deg)/180
+}
+// ----------------------------------------------------
+
+// -------------------------- TRANSFO DE DEGRES/RADIANS 
+function radToDeg(rad)
+{
+   // return (Math.PI*deg)/180
+   return (rad*180)/Math.PI
+}
+// ----------------------------------------------------
--- html/js/Player.js
+++ html/js/Player.js
@@ -1,21 +1,174 @@
 Player = function(game, canvas) {
-    // La scène du jeu
-    this.scene = game.scene;
+    // _this est l'accès à la caméraà l'interieur de Player
+    var _this = this;
 
-    // Initialisation de la caméra
-    this._initCamera(this.scene, canvas);
+    // Le jeu, chargé dans l'objet Player
+    this.game = game;
+
+    this.angularSensibility = 200.0;
+    this.speed = 1.0;
+
+    // Si le tir est activée ou non
+    this.weponShoot = false;
+
+    // Axe de mouvement X et Z
+    this.axisMovement = [false,false,false,false];
+
+    window.addEventListener("keyup", function(evt) {
+        
+        switch(evt.key){
+            case 'w':
+            _this.camera.axisMovement[0] = false;
+            break;
+            case 's':
+            _this.camera.axisMovement[1] = false;
+            break;
+            case 'a':
+            _this.camera.axisMovement[2] = false;
+            break;
+            case 'd':
+            _this.camera.axisMovement[3] = false;
+            break;
+        }
+    }, false);
     
+    // Quand les touches sont relachés
+    window.addEventListener("keydown", function(evt) {
+        switch(evt.key){
+            case 'w':
+            _this.camera.axisMovement[0] = true;
+            break;
+            case 's':
+            _this.camera.axisMovement[1] = true;
+            break;
+            case 'a':
+            _this.camera.axisMovement[2] = true;
+            break;
+            case 'd':
+            _this.camera.axisMovement[3] = true;
+            break;
+        }
+    }, false);
+
+    window.addEventListener("mousemove", function(evt) {
+        if(_this.rotEngaged === true){
+            _this.camera.rotation.y+=evt.movementX * 0.001 * (_this.angularSensibility / 250);
+            var nextRotationX = _this.camera.rotation.x + (evt.movementY * 0.001 * (_this.angularSensibility / 250));
+            if( nextRotationX < degToRad(90) && nextRotationX > degToRad(-90)){
+                _this.camera.rotation.x+=evt.movementY * 0.001 * (_this.angularSensibility / 250);
+            }
+        }
+    }, false);
+
+    // On récupère le canvas de la scène 
+    var canvas = this.game.scene.getEngine().getRenderingCanvas();
+
+    // On affecte le clic et on vérifie qu'il est bien utilisé dans la scène (_this.controlEnabled)
+    canvas.addEventListener("mousedown", function(evt) {
+        if (_this.controlEnabled && !_this.weponShoot) {
+            _this.weponShoot = true;
+            _this.handleUserMouseDown();
+        }
+    }, false);
+
+    // On fait pareil quand l'utilisateur relache le clic de la souris
+    canvas.addEventListener("mouseup", function(evt) {
+        if (_this.controlEnabled && _this.weponShoot) {
+            _this.weponShoot = false;
+            _this.handleUserMouseUp();
+        }
+    }, false);
+    
+    // Initialisation de la caméra
+    this._initCamera(this.game.scene, canvas); 
+
+    // Le joueur doit cliquer dans la scène pour que controlEnabled soit changé
+    this.controlEnabled = false;
+
+    // On lance l'event _initPointerLock pour checker le clic dans la scène
+    this._initPointerLock(); 
 };
 
+
 Player.prototype = {
+    _initPointerLock : function() {
+        var _this = this;
+        
+        // Requete pour la capture du pointeur
+        var canvas = this.game.scene.getEngine().getRenderingCanvas();
+        canvas.addEventListener("click", function(evt) {
+            canvas.requestPointerLock = canvas.requestPointerLock || canvas.msRequestPointerLock || canvas.mozRequestPointerLock || canvas.webkitRequestPointerLock;
+            if (canvas.requestPointerLock) {
+                canvas.requestPointerLock();
+            }
+        }, false);
+    
+        // Evenement pour changer le paramètre de rotation
+        var pointerlockchange = function (event) {
+            _this.controlEnabled = (document.mozPointerLockElement === canvas || document.webkitPointerLockElement === canvas || document.msPointerLockElement === canvas || document.pointerLockElement === canvas);
+            if (!_this.controlEnabled) {
+                _this.rotEngaged = false;
+            } else {
+                _this.rotEngaged = true;
+            }
+        };
+        
+        // Event pour changer l'état du pointeur, sous tout les types de navigateur
+        document.addEventListener("pointerlockchange", pointerlockchange, false);
+        document.addEventListener("mspointerlockchange", pointerlockchange, false);
+        document.addEventListener("mozpointerlockchange", pointerlockchange, false);
+        document.addEventListener("webkitpointerlockchange", pointerlockchange, false);
+    },
+
     _initCamera : function(scene, canvas) {
         // On crée la caméra
-        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
-
-        // On demande à la caméra de regarder au point zéro de la scène
+        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(-20, 5, 0), scene);
+        
+        // On demande a la caméra de regarder au point zéro de la scène
         this.camera.setTarget(BABYLON.Vector3.Zero());
+        
+        // Axe de mouvement X et Z
+        this.camera.axisMovement = [false,false,false,false];
+    
+        // Appel de la création des armes
+        this.camera.weapons = new Weapons(this);
+    
+        // Si le joueur est en vie ou non
+        this.isAlive = true;
+    },
 
-        // On affecte le mouvement de la caméra au canvas
-        this.camera.attachControl(canvas, true);
-    }
+    _checkMove : function(ratioFps) {
+        let relativeSpeed = this.speed / ratioFps;
+        if(this.camera.axisMovement[0]){
+            this.camera.position = new BABYLON.Vector3(this.camera.position.x + (Math.sin(this.camera.rotation.y) * relativeSpeed),
+                this.camera.position.y,
+                this.camera.position.z + (Math.cos(this.camera.rotation.y) * relativeSpeed));
+        }
+        if(this.camera.axisMovement[1]){
+            this.camera.position = new BABYLON.Vector3(this.camera.position.x + (Math.sin(this.camera.rotation.y) * -relativeSpeed),
+                this.camera.position.y,
+                this.camera.position.z + (Math.cos(this.camera.rotation.y) * -relativeSpeed));
+        }
+        if(this.camera.axisMovement[2]){
+            this.camera.position = new BABYLON.Vector3(this.camera.position.x + Math.sin(this.camera.rotation.y + degToRad(-90)) * relativeSpeed,
+                this.camera.position.y,
+                this.camera.position.z + Math.cos(this.camera.rotation.y + degToRad(-90)) * relativeSpeed);
+        }
+        if(this.camera.axisMovement[3]){
+            this.camera.position = new BABYLON.Vector3(this.camera.position.x + Math.sin(this.camera.rotation.y + degToRad(-90)) * - relativeSpeed,
+                this.camera.position.y,
+                this.camera.position.z + Math.cos(this.camera.rotation.y + degToRad(-90)) * - relativeSpeed);
+        }
+    },
+
+    handleUserMouseDown : function() {
+        if(this.isAlive === true){
+            this.camera.weapons.fire();
+        }
+    },
+    handleUserMouseUp : function() {
+        if(this.isAlive === true){
+            this.camera.weapons.stopFire();
+        }
+    },
 };
