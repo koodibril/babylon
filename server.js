603a254221ce2c1d60008fec1d35e472a8fd2853
AlphaBril <megack79@gmail.com>
2021-07-14 13:19:54 +0200

    node will listen on 80 now

--- server.js
+++ server.js
@@ -293,7 +293,7 @@ setInterval(function(){
 
 // ================================================
 // START LISTENING ON THE PORT
-server.listen(3000, function () {
+server.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
 
